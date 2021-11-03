33289960abfd68faa2f96da52aa2d46dbaa9c6f3
EmaOlay <olayemanuel@gmail.com>
2021-11-03 18:59:53 -0300

    Update TS8

--- avance TS8.py
+++ avance TS8.py
@@ -173,3 +173,62 @@ figura+=1
 #plt.semilogy(f_welch,Pxx_den)
 plt.plot(f_welch,Pxx_den)
 plt.xlim(0.240,0.260)
+
+################################
+###Calculo del periodograma con 3 dB, zero padding y otra ventana
+################################
+#Creo un nuevo vector f para los calculos
+f_padded=np.arange(0,fs,fs/(10*N))
+
+#Creo un nuevo x_0 ahora ventaneado con algo distinto de la rectangular implicita
+#Blackman
+Blackman=np.blackman(N)
+x_0_Blackman=x_0*Blackman.reshape(N,1)
+
+#calculo el periodograma normal
+Pp=(1/N)*np.abs((fft(x_0_Blackman,n=10*N,axis=0)))**2
+
+
+#Metodo de Welch
+f_welch,Pxx_den = sig.welch(x_0,window='blackman',nperseg=500,nfft=10*N,axis=0)
+
+###Grafico las salidas
+plt.figure(figura)
+figura+=1
+##Restrinjo las salidas para ver mas claramente los efectos del calculo
+#plt.semilogy(f,Pp)
+
+plt.plot(f_padded,Pp)
+plt.xlim(240,260)
+
+plt.figure(figura)
+figura+=1
+#plt.semilogy(f_welch,Pxx_den)
+plt.plot(f_welch,Pxx_den)
+plt.xlim(0.240,0.260)
+
+################################
+###Calculo del periodograma con 10 dB, zero padding y otra ventana
+################################
+
+x_1_Blackman=x_1*Blackman.reshape(N,1)
+#calculo el periodograma normal
+Pp=(1/N)*np.abs((fft(x_1_Blackman,n=10*N,axis=0)))**2
+
+#Metodo de Welch
+f_welch,Pxx_den = sig.welch(x_1,window='blackman',nperseg=500,nfft=10*N,axis=0)
+
+##Grafico las salidas
+plt.figure(figura)
+figura+=1
+##En este grafico restrinjo el eje x ya que no queremos la otra mitad
+#plt.semilogy(f,Pp)
+plt.plot(f_padded,Pp)
+plt.xlim(240,260)
+
+
+plt.figure(figura)
+figura+=1
+#plt.semilogy(f_welch,Pxx_den)
+plt.plot(f_welch,Pxx_den)
+plt.xlim(0.240,0.260)
